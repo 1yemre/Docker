a9324e2cbd160123e8d0bcaf1164a0a7fa4e317f
Emre  Enes Yenen <107006546+1yemre@users.noreply.github.com>
2025-02-08 10:18:34 +0300

    Update Helloworld.java

--- Docker_image_Build/DockerFileApp4/Helloworld.java
+++ Docker_image_Build/DockerFileApp4/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Adınızı girin:");
+        System.out.println("Adinizi girin:");
         String name = scanner.nextLine();
         
         System.out.println("Merhaba, " + name + "!");
